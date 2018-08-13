ecd3037ce48398f765af20a90600ec833df809a8
Cristian Gianti <gianticristian@gmail.com>
2018-08-13 20:08:37 -0300

    Se crea base de clase Enemigos

---
+++ source/Enemy.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxSprite;
+import flixel.system.FlxAssets.FlxGraphicAsset;
+
+
+class Enemy extends FlxSprite 
+{
+
+	public function new(?X:Float=0, ?Y:Float=0, ?SimpleGraphic:FlxGraphicAsset) 
+	{
+		super(X, Y, SimpleGraphic);
+		
+		
+		
+	}
+	
+}
